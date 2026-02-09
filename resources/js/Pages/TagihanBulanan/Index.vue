b2cb1b2338febd788dbdce5135bc3ea81bdcd705
alpaenf <alfaenf_@gmail.com>
2026-02-10 01:02:53 +0700

    halamanlaporannnnn

--- resources/js/Pages/TagihanBulanan/Index.vue
+++ resources/js/Pages/TagihanBulanan/Index.vue
@@ -208,7 +208,7 @@
                                                 </svg>
                                                 Pending Konfirmasi
                                             </span>
-                                            <span v-if="item.tagihan?.status_bayar === 'SUDAH_BAYAR'" class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-blue-100 text-blue-800">
+                                            <span v-else-if="item.tagihan?.status_bayar === 'SUDAH_BAYAR'" class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-blue-100 text-blue-800">
                                                 <svg class="w-3 h-3 mr-1" fill="currentColor" viewBox="0 0 20 20">
                                                     <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
                                                 </svg>
