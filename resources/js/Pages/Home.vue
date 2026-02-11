b02620d0f38dc6b8e5b5439f829cf92a86820c7e
alpaenf <alfaenf_@gmail.com>
2026-02-11 20:06:07 +0700

    footer

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -829,15 +829,15 @@
                         <div class="space-y-3 text-sm">
                             <div class="flex items-start gap-3">
                                 <svg class="w-5 h-5 text-blue-500 mt-0.5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
-                                <span class="text-gray-400">Jl. Raya Ciwuni, Desa Ciwuni<br>Kec. Cilongok, Kab. Banyumas<br>Jawa Tengah 53162</span>
+                                <span class="text-gray-400">Jalan raya pasar jagang No 15<br>Rt 05 Rw 03 Desa ciwuni<br>Kecamatan kesugihan Kabupaten cilacap</span>
                             </div>
                             <div class="flex items-center gap-3">
                                 <svg class="w-5 h-5 text-blue-500" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path></svg>
-                                <span class="text-gray-400">+62 812-3456-7890</span>
+                                <span class="text-gray-400">085228357400</span>
                             </div>
                             <div class="flex items-center gap-3">
                                 <svg class="w-5 h-5 text-blue-500" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path></svg>
-                                <span class="text-gray-400">kpspams.ciwuni@gmail.com</span>
+                                <span class="text-gray-400">Kpspamsdamarwulan@gmail.com</span>
                             </div>
                         </div>
                     </div>
